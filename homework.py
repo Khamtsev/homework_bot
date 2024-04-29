5ef1ec76fde6b9edcb973b4ee006396c400a80ce
Khamtsev <dhamcev@yandex.ru>
2024-04-29 14:13:54 +0300

    Add fixes according to PEP8

--- homework.py
+++ homework.py
@@ -34,7 +34,7 @@ HOMEWORK_VERDICTS = {
 }
 
 
-def check_tokens() -> list[str]:
+def check_tokens() -> None:
     """Проверка наличия необходимых токенок."""
     TOKENS = {
         'PRACTICUM_TOKEN': PRACTICUM_TOKEN,
@@ -46,7 +46,10 @@ def check_tokens() -> list[str]:
         if not TOKENS[token]:
             logging.critical(f'Отсутствует токен {token}')
             missing_tokens.append(token)
-    return missing_tokens
+    if missing_tokens != []:
+        missing_tokens = ', '.join(missing_tokens)
+        logging.critical(f'Отсутствуют токены {missing_tokens}')
+        sys.exit()
 
 
 def send_message(bot: telegram.Bot, message: str) -> None:
@@ -106,52 +109,44 @@ def parse_status(homework: dict) -> str:
 
 def main() -> None:
     """Основная логика работы бота."""
-    missing_tokens = check_tokens()
-    if missing_tokens != []:
-        missing_tokens = ', '.join(missing_tokens)
-        logging.critical(f'Отсутствуют токены {missing_tokens}')
-        sys.exit()
-    else:
+    check_tokens()
+    bot = telegram.Bot(token=TELEGRAM_TOKEN)
+    send_message(bot, 'Начало работы')
+    timestamp = int(time.time())
+    last_status = ''
+    while True:
         try:
-            bot = telegram.Bot(token=TELEGRAM_TOKEN)
-        except telegram.TelegramError as error:
-            logging.error(f'Не удалось подключиться к боту: {error}')
-        send_message(bot, 'Начало работы')
-        timestamp = int(time.time())
-        last_status = ''
-        while True:
-            try:
-                response = get_api_answer(timestamp)
-                timestamp = int(time.time())
-                homeworks = check_response(response)
-                if homeworks:
-                    new_status = parse_status(homeworks[0])
-                    if new_status != last_status:
-                        send_message(bot, new_status)
-                        last_status = new_status
-                    else:
-                        logging.debug('Обновлений статуса не было')
-            except ConnectionError as error:
-                message = f'Сбой в работе программы: {error}'
-                logging.error(message)
-                send_message(bot, message)
-            except TypeError as error:
-                message = f'Сбой в работе программы: {error}'
-                logging.error(message)
-                send_message(bot, message)
-            except KeyError as error:
-                message = f'Сбой в работе программы: {error}'
-                logging.error(message)
-                send_message(bot, message)
-            except WrongStatus as error:
-                message = f'Сбой в работе программы: {error}'
-                logging.error(message)
-                send_message(bot, message)
-            except Exception as error:
-                message = f'Сбой в работе программы: {error}'
-                logging.error(message)
-            finally:
-                time.sleep(RETRY_PERIOD)
+            response = get_api_answer(timestamp)
+            timestamp = int(time.time())
+            homeworks = check_response(response)
+            if homeworks:
+                new_status = parse_status(homeworks[0])
+                if new_status != last_status:
+                    send_message(bot, new_status)
+                    last_status = new_status
+                else:
+                    logging.debug('Обновлений статуса не было')
+        except ConnectionError as error:
+            message = f'Сбой в работе программы: {error}'
+            logging.error(message)
+            send_message(bot, message)
+        except TypeError as error:
+            message = f'Сбой в работе программы: {error}'
+            logging.error(message)
+            send_message(bot, message)
+        except KeyError as error:
+            message = f'Сбой в работе программы: {error}'
+            logging.error(message)
+            send_message(bot, message)
+        except WrongStatus as error:
+            message = f'Сбой в работе программы: {error}'
+            logging.error(message)
+            send_message(bot, message)
+        except Exception as error:
+            message = f'Сбой в работе программы: {error}'
+            logging.error(message)
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
